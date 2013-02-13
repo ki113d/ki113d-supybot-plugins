65a432ec2c11038c46f38fcfe1fe1ed057f8a500
ki113d <ki113d.69@gmail.com>
2013-02-13 07:15:32 +0100

    Added base classes and begun working on deck mechanics.

--- Uno/README.txt
+++ Uno/README.txt
@@ -1,1 +1,3 @@
 Atempted replacement of the original, buggy as fuck, Uno plugin for supybot!
+
+W.I.P currently not fully implemented!
--- Uno/plugin.py
+++ Uno/plugin.py
@@ -35,7 +35,10 @@ import supybot.ircutils as ircutils
 import supybot.callbacks as callbacks
 from supybot.i18n import PluginInternationalization, internationalizeDocstring
 
+import random
+
 _ = PluginInternationalization('Uno')
+
 
 @internationalizeDocstring
 class Uno(callbacks.Plugin):
@@ -43,19 +46,123 @@ class Uno(callbacks.Plugin):
     This should describe *how* to use this plugin."""
     pass
 
+    def start(self, irc):
+        
+        pass
+    start = wrap(start)
+
 
 Class = Uno
+
+class Game(object):
+    pass
 
 class Table(object):
 
     def __init__(self):
-        self._player_list = set()
+        self._move = 0
+        self._players = dict()
+        self._info = {'creater', '';}
 
     @property
-    def player_list(self):
-        return self._player_list
+    def players(self):
+        return self._players
 
     def add_user(self, player):
-        pass
+        if player.lower() not in self._players.keys():
+            self._players[player.lower()] = Player(name, len(self._players))
+
+    def next_move(self):
+        if len(self._players) < 2:
+
+
+class Player(object):
+
+    def __init__(self, name, id):
+        self._name = name
+        self._id = id
+
+class Card(object):
+    """
+        Base class for cards.
+    """
+    pass
+
+class Number_Card(Card):
+    def __init__(self, number, colour):
+        self._number = number
+        self._colour = colour
+
+    @property
+    def number(self):
+        return self._number
+
+    @property
+    def colour(self):
+        return self._colour
+
+    def __str__(self):
+        return '{0}:{1}'.format(self._number, self._colour)
+
+class Special_Card(Card):
+    def __init__(self, special, colour):
+        self._special = special
+        self._colour = colour
+    
+    @property
+    def special(self):
+        return self._special
+    
+    @property
+    def colour(self):
+        return self._colour
+    
+    def __str__(self):
+        return '{0}:{1}'.format(self._special, self._colour)
+
+class Deck(object):
+
+    def __init__(self):
+        self._list = list()
+
+    def init_list(self):
+        # Set up deck with coloured cards.
+        for colour in (Colours.BLUE, Colours.GREEN, Colours.RED, Colours.YELLOW):
+            for number in xrange(10):
+                self._list.exted(Number_Card(number, colour)
+                if number >  0:
+                    self._list.extend(Number_Card(number, colour)
+            for special in (Specials.DRAW_TWO, Specials.REVERSE, Specials.SKIP):
+                for number in xrange(2):
+                    self._list.extend(Special_Card(special, colour))
+        # And finally wild cards.
+        for special in (Specials.WILD, Special.WILD_DRAW_FOUR):
+            for x in xrange(4):
+                self._list.extend(Special_Card(special, Colours.NONE)
+
+
+    def shuffle(self):
+        random.shuffle(self._list, random.random)
+
+class Error():
+    '''
+        Base class for all exceptions.
+    '''
+    pass
+
+def enum(**enums):
+    return type('Emum', (), enums)
+
+Colours = enum(BLUE='Blue',
+               GREEN='Green',
+               RED='Red',
+               YELLOW='Yellow',
+               NONE='None')
+
+Specials = enum(DRAW_TWO='Draw Two',
+                REVERSE='Reverse',
+                SKIP='Skip',
+                WILD='Wild',
+                WILD_DRAW_FOUR= 'Wild Draw Four')
 
 # vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
